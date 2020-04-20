e02a2d7c2a8a1dded40bb45782764c483167927b
jiajia <1576407287@qq.com>
2020-04-20 10:38:11 +0800

    git commit annotation

---
+++ src/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sunny;
+
+use Doctrine\Common\Annotations\Annotation\Attributes;
+use Sunny\Annotations\JiajiaAnnotation;
+
+/**
+ * @JiajiaAnnotation(name="My name is Jiajia")
+ */
+class Test
+{
+
+}
